3dbc713cef3ad613e793332b12baaf9383f6082e
Ashton Regensberg <aregensberg@gmail.com>
2020-05-21 17:36:12 -0600

    added js and container

---
+++ public_html/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	$("#contact").validate({
+		debug: true,
+		errorClass:"alert alert-danger",
+		errorLableContainer: "output-area",
+		errorElement: "div",
+
+		rules: {
+			name: {
+				required:true
+			},
+			email: {
+				email: true,
+				required: true
+			},
+			message: {
+				required: true,
+				maxLength: 2000
+			}
+		},
+		messages: {
+			name: {
+				required:"Name is required, please input a Name"
+			},
+			email: {
+				required: "Email is required, please input an email address",
+				email: "Invalid Email"
+			},
+			message: {
+				required: "Please write a message in the message field",
+				maxLength: "You've exceeded 2000 characters, please reduce your message"
+			}
+		},
+		submitHandler: function (form) {
+			$("#contact").ajxSubmit({
+				type: "POST",
+				url: $("#contact").attr("action"),
+				success: function (ajaxOutput) {
+					$("output-area").css("display", "")
+					$("output-area").html(ajaxOutput)
+
+					if($(".alert-success").length >= 1) {
+				$("#contact")[0].reset()
+			}
+				}
+			})
+		}
+	})
+})

b740ff5e4f9619092c8c4b08c10b8fa5f2459927
Ashton Regensberg <aregensberg@gmail.com>
2020-06-02 22:36:33 -0600

    added contact form files for recaptcha/docker/yml

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const mailgun = require('mailgun-js')
 const bodyParser =require('body-parser')
 const{check, validationResult} = require('express-validator')
+const Recaptcha = require('express-recaptcha').RecaptchaV2
 
 const app = express()
 
@@ -11,6 +11,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+const recaptcha = new Recaptcha(process.env.RCAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 const indexRoute = express.Router()
 
 const requestValidation = [
@@ -19,16 +20,25 @@ const requestValidation = [
 	check ('message', 'A message is a required to send an email').not().isEmpty().trim().escape().isLength({max:2000})
 ]
 
-indexRoute.route("/apis")
+indexRoute.route('/apis')
 	.get((request,response) => {
 		return response.json({status:200})
-})
-	.post(requestValidation, (request, response) => {
-		response.append('Content-Type', 'text/html')
-		response.append('Access-Control-Allow-Origin', ['*'])
-		const domain = process.env.MAILGUN_DOMAIN
-		const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
+}).post(recaptcha.middleware.verify, requestValidation, (request, response) => {
+	response.append('Content-Type', 'text/html')
 
+	if (request.recaptcha.error) {
+		return response.send(`<div class='alert alert-danger' role='alert'><strong>Try Again!</strong>There was an error with Recaptcha</div>`)
+	}
+
+	const errors = validationResult(request)
+
+	if (!errors.isEmpty()) {
+		const currentError = errors.array()[0]
+		return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Try Again!!</strong> ${currentError.msg}</div>`))
+	}
+
+	const domain = process.env.MAILGUN_DOMAIN
+	const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
 	const {name, email, message} = request.body
 
 	const mailgunData = {
@@ -40,17 +50,9 @@ indexRoute.route("/apis")
 
 	mg.messages().send(mailgunData, (error) => {
 		if (error) {
-			return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Oh snap!</strong> Unable to send email error with email sender</div>`))
+			return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'><strong>Try Again!</strong> Unable to send email, error with email sender</div>`))
 		}
 	})
-
-
-	const errors = validationResult(request)
-
-	if(!errors.isEmpty()) {
-		const currentError = errors.array()[0]
-		return response.json('bad request: ${currentError.msg}')
-	}
 
 	return response.send(Buffer.from("<div class ='alert alert-success' role='alert'>Email successfully sent.</div>"))
 })
